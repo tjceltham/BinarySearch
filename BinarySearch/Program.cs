f5fd330357fb55b531fc47b3c2eefa5fd6935019
Tony Collins <tjc@eltham-college.org.uk>
2021-01-07 12:19:44 +0000

    Add project files.

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = { 1, 4, 7,9,56,78,150,345 };
+            int num = 0;
+            string numStr = "";
+            Console.WriteLine("Enter the number you would like to search for");
+            numStr = Console.ReadLine();
+            num = Convert.ToInt32(numStr);
+
+            // add a Binary search code here
+            // Print out a message stating if the number is in the list and if it is 
+            // in the list print out the index of the number
+        }
+    }
+}
